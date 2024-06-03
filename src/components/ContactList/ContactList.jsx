e9def1f2a08243cc996edfebd06692eef43ba07b
OlenaPanf <elena.el@ukr.net>
2024-06-03 23:39:57 +0300

    add

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,12 @@
 import css from './ContactList.module.css'
 import Contact from "../Contact/Contact"
 
-export default function ContactList({ contacts, onDelete }) {
+export default function ContactList({ contacts }) {
     return (
     <ul className={css.list}>
       {contacts.map((contact) => (
         <li className={css.item} key={contact.id}>
-          <Contact data={contact} onDelete={onDelete} />
+          <Contact data={contact} />
         </li>
       ))}
     </ul>
